82ca56ea7ac689b237abb8d6932ab19d700ffb2f
faisalzone <faisalzone@gmail.com>
2021-01-13 03:28:46 +1100

    user chart for each user

--- skills/urls.py
+++ skills/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import skill_view
+from .views import skill_view, UserChartView
 
 
 app_name = 'skills'
 
 
 urlpatterns = [
+    path('<profile_id>/', UserChartView.as_view(), name='user-skills'),
     path('', skill_view, name='my-skills'),
 ]
--- skills/views.py
+++ skills/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.forms import inlineformset_factory
+from django.views.generic import TemplateView
+
 from .models import Skill
 from profiles.models import Profile
 
@@ -8,6 +10,21 @@ from profiles.models import Profile
 
 def home_view(request):
     return render(request, 'skills/home.html', {})
+
+
+class UserChartView(TemplateView):
+    template_name = 'skills/user_skills.html'
+
+    def get_object(self):
+        pk = self.kwargs.get('profile_id')
+        return pk
+
+    def get_context_data(self, **kwargs):
+        context = super(UserChartView, self).get_context_data(**kwargs)
+        profile = Profile.objects.get(name__id=self.get_object())
+        qs = profile.skill_set.all()
+        context["qs"] = qs
+        return context
 
 
 def skill_view(request):
